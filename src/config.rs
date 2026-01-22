4aa1640e9d99595f6331f01bbc26e1d33c56e016
jtong1231 <jtong5995@gmail.com>
2026-01-22 14:11:53 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,12 +100,12 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["154.82.67.181"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["154.82.66.62"];
 pub const RS_PUB_KEY: &str = "YYy4QBgCfGDLI79kVRKLdZIzyCvL1KMb4+u5lZ5bmF0=";
-pub const RENDEZVOUS_PORT: i32 = 38356;
-pub const RELAY_PORT: i32 = 38357;
-pub const WS_RENDEZVOUS_PORT: i32 =38358;
-pub const WS_RELAY_PORT: i32 = 38359;
+pub const RENDEZVOUS_PORT: i32 = 38456;
+pub const RELAY_PORT: i32 = 38457;
+pub const WS_RENDEZVOUS_PORT: i32 =38458;
+pub const WS_RELAY_PORT: i32 = 38459;
 
 macro_rules! serde_field_string {
     ($default_func:ident, $de_func:ident, $default_expr:expr) => {
